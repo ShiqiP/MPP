187df4177323403810c6fca081acd8070660fe3a
ahmedm3377 <ahmedm3377@yahoo.com>
2024-10-08 20:43:53 -0500

    Update Polygon.java

--- src/Lab7/prob2/Polygon.java
+++ src/Lab7/prob2/Polygon.java
@@ -1,6 +1,15 @@
-
 public interface Polygon extends ClosedCurve{
+	
 	default double[] getSides() {
 		return null;
 	}
+	
+	default double computePerimeter() {
+		double perimeter =  0.0;
+		double[]  sides  = getSides();
+		for(double side:sides)
+			perimeter+=side;
+		return perimeter;
+	}
+
 }
