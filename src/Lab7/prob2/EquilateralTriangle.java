7c227a2ae780f8f52f8b8e55ea45a995b3a31f6d
ahmedm3377 <ahmedm3377@yahoo.com>
2024-10-08 20:45:15 -0500

    Update EquilateralTriangle.java

--- src/Lab7/prob2/EquilateralTriangle.java
+++ src/Lab7/prob2/EquilateralTriangle.java
@@ -6,11 +6,14 @@ public class EquilateralTriangle implements Polygon {
 		this.side = side;
 	}
 
-	@Override
-	public double computePerimeter() {
-		
-		return 3*side;
-	}
+//	@Override
+//	public double computePerimeter() {
+//		double perimeter =  0.0;
+//		double[]  sides  = getSides();
+//		for(double side:sides)
+//			perimeter+=side;
+//		return perimeter;
+//	}
 
 	public double[] getSides() {
 		return new double[] {side, side, side};
