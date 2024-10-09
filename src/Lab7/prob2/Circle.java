6c7aea1c647c3934c7824d4f677837c1a450b4c1
ahmedm3377 <ahmedm3377@yahoo.com>
2024-10-08 20:14:19 -0500

    Update Circle.java

--- src/Lab7/prob2/Circle.java
+++ src/Lab7/prob2/Circle.java
@@ -1,31 +1,18 @@
 
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class DataMiner {
-	List<ClosedCurve> objects = new ArrayList<>();
-	public static void main(String[] args) {
-		DataMiner dm = new DataMiner();
-		dm.objects.add(new Rectangle(5,7));
-		dm.objects.add(new Rectangle(2,9));
-		dm.objects.add(new Circle(6));
-		dm.objects.add(new EquilateralTriangle(5));
-		dm.objects.add(new Ellipse(2,5));
-
-		
-		System.out.println(dm.computeAveragePerimeter());
+public class Circle implements ClosedCurve {
+	private double radius;
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+	public double getRadius() {
+		return radius;
+	}
+	public void setRadius(double radius) {
+		this.radius = radius;
 	}
 	
-	//OO (good) way of performing computation
-	public double computeAveragePerimeter() {
-		if(objects == null || objects.isEmpty()) return 0.0;
-		double sum = 0.0;
-		for(int i = 0; i < objects.size(); ++i) {
-			sum += objects.get(i).computePerimeter();
-		}
-		return sum/objects.size();
+	@Override
+	public double computePerimeter() {
+		return 2 * Math.PI * radius;
 	}
-
 }
-
