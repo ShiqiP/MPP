4ef3682da8b8617d9520033600604e004de9eee4
Ahmed Ibrahim <ahmedm3377@yahoo.com>
2024-10-13 20:12:05 -0500

    Update Prob1.java

--- src/Lab9/prob1/Prob1.java
+++ src/Lab9/prob1/Prob1.java
@@ -1,1 +1,35 @@
+package prob1;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Prob1 {
+
+	public static void main(String[] args) {
+
+		List<String> words = new ArrayList<>();
+		words.add("Java");
+		words.add("Python");
+		words.add("Javascript");
+		words.add("Java");
+		
+		Prob1 obj = new Prob1();
+		int count = obj.countWords( words, 'J', 'c', 4);
+		System.out.println(count);
+		
+	     
+	}
+	
+	
+	
+	public int countWords(List<String> words, char c, char d, int len)
+	 {
+		 long count = words.stream()
+		 .filter(x->x.length()==len)
+		 .filter(x->x.contains(String.valueOf(c)))
+		 .filter(x->!x.contains(String.valueOf(d)))
+		 .count();
+		 return (int)count;
+	 }
+
+}
